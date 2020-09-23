16960539fb865b3f833b619bd1e55e731c43ae7a
suntoss1997 <suntoss1997@gmail.com>
2020-09-23 20:26:19 +0545

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const forecast=require('./utils/forecast');
 const creator='Santosh Acharya';
 
 const app=express();
+const port= process.env.PORT || 3000
 
 //Define paths for express config
 const publicDirectory=path.join(__dirname, '../public');
@@ -106,8 +107,8 @@ res.render('404', {
 })
 
 
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000.');
+app.listen(port, ()=>{
+    console.log('Server is up on port '+  port);
 });
 
 //app.com
